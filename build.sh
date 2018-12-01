b62ae404e491ed10100489d015494a814a4a2102
suguru <sgr.inoue@gmail.com>
2018-12-01 13:32:04 +0900

    don't uglify when develop mode

--- build.sh
+++ build.sh
@@ -9,10 +9,15 @@ cp -r ./public/* ./dist/
 js="dist/app.js"
 min="dist/app.min.js"
 
-elm make ./src/Main.elm --optimize --output=$js $@
+if [ "$PROD" = "true" ]; then
+  elm make ./src/Main.elm --optimize --output=$js $@
 
-uglifyjs $js --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' | uglifyjs --mangle --output=$min
+  uglifyjs $js --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' | uglifyjs --mangle --output=$min
 
-echo "Initial size: $(cat $js | wc -c) bytes  ($js)"
-echo "Minified size:$(cat $min | wc -c) bytes  ($min)"
-echo "Gzipped size: $(cat $min | gzip -c | wc -c) bytes"
+  echo "Initial size: $(cat $js | wc -c) bytes  ($js)"
+  echo "Minified size:$(cat $min | wc -c) bytes  ($min)"
+  echo "Gzipped size: $(cat $min | gzip -c | wc -c) bytes"
+
+else
+  elm make ./src/Main.elm --output=$min $@
+fi
